6f808388158bb5dd02e04533cc3bc283ca3bb7d5
gregpedis <trfalgar1618@hotmail.com>
2025-04-16 01:39:39 +0200

    Deploy

--- DnD10k.Client/src/app/models/effect.ts
+++
@@ -1,9 +0,0 @@
-export class Effect {
-    id: number;
-    description: string;
-
-    constructor(id: number = 0, description: string = '') {
-        this.id = id;
-        this.description = description;
-    }
-}
